23896dd0418f26b3becd725cbdfbab0ec5024c8f
TRON17 <hkhare360@gmail.com>
2025-07-23 14:49:14 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -2,3 +2,7 @@ const socket = io()
 socket.on('countUpdated',(count)=>{
   console.log('The count has been updated!',count)
 })
+document.queryselector('#increment').addEventListner('click',()=>{
+console.log('Clicked')
+  socket.emit('increment')
+})
