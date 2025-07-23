21dcfa45db63e9f4d533c9542bb9786aee526038
TRON17 <hkhare360@gmail.com>
2025-07-23 17:05:35 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,4 +1,18 @@
 const socket = io()
+socket.on('message',(message)=>{
+console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit',(e)=>{
+  e.preventDefault()
+  const message = document.querySelector('input').value
+  socket.email('sendMessage',message)
+  socket.on('sendMessage',(message)=>{
+    io.emit('message',message)
+    const message = e.target.elements.message.value
+    socket.email('sendMessage',message)
+})
+
 socket.on('countUpdated',(count)=>{
   console.log('The count has been updated!',count)
 })
