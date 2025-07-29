008955d1f6e8e7284e119785c10ab6cd2e6759a1
TRON17 <hkhare360@gmail.com>
2025-07-29 13:15:28 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,4 +1,11 @@
 const socket = io()
+//element
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+
+
 socket.on('message',(message)=>{
 console.log(message)
 })
@@ -12,7 +19,19 @@ document.querySelector('#message-form').addEventListener('submit',(e)=>{
     const message = e.target.elements.message.value
     socket.email('sendMessage',message)
 })
-
+$messageForm.addEventListner('submit',(e)=>{
+e.preventDefault()
+  $messageFormButton.setAttribute('disabled','disabled')
+  const message=e.target.elements.message.value
+  socket.emit('sendMessage',message,(error)=>{
+    $messageFormButton.removeAttribute('disabled')
+    $messageFormInput.value=''
+    $messageFormInput.focus()
+if(error){
+  return console.log(error)
+  }
+    console.log('Message Delivered')
+  })
 socket.on('countUpdated',(count)=>{
   console.log('The count has been updated!',count)
 })
